372c6a7f147150a13bd8668c8e77dfc444af2794
nholzbach <81358704+nholzbach@users.noreply.github.com>
2023-09-06 21:44:47 +0200

    Added snellius run files via upload

---
+++ snellius_runs/hysplit_data/automate_plume_viz_edit.py
@@ -0,0 +1,358 @@
+"""
+Automate the plume simulation using hysplit model
+"""
+
+import os, re, datetime, json, pytz, subprocess, time, shutil, requests, traceback
+import numpy as np
+# from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
+import urllib.parse
+import urllib.request
+from multiprocessing.dummy import Pool
+from os import listdir
+from os.path import isfile, join, isdir
+from zipfile import ZipFile
+# import cv2 as cv
+# from PIL import Image, ImageFont, ImageDraw
+from utils import subprocess_check
+from pardump_util import findInFolder, create_multisource_bin
+from cached_hysplit_run_lib_edit import getMultiHourDispersionRunsParallel, parse_eastern, HysplitModelSettings, InitdModelType, CachedDispersionRun
+
+
+hysplit_root = "/home/nholzbach/hysplit/"
+
+# do I need this? check where used
+def exec_ipynb(filename_or_url):
+    """Load other ipython notebooks and import their functions"""
+    nb = (requests.get(filename_or_url).json() if re.match(r'https?:', filename_or_url) else json.load(open(filename_or_url)))
+    if(nb['nbformat'] >= 4):
+        src = [''.join(cell['source']) for cell in nb['cells'] if cell['cell_type'] == 'code']
+    else:
+        src = [''.join(cell['input']) for cell in nb['worksheets'][0]['cells'] if cell['cell_type'] == 'code']
+    tmpname = '/tmp/%s-%s-%d.py' % (os.path.basename(filename_or_url),
+                                    datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'),
+                                    os.getpid())
+    src = '\n\n\n'.join(src)
+    open(tmpname, 'w').write(src)
+    code = compile(src, tmpname, 'exec')
+    exec(code, globals())
+
+# this is fine
+def get_start_end_time_list(start_date_eastern, end_date_eastern, duration=24, offset_hours=3):
+    """
+    Given starting and ending date string, get a list of starting and ending datetime objects
+
+    Input:
+        start_date_eastern: the date to start in EST time, e.g., "2019-01-01"
+        end_date_eastern: the date to end in EST time, e.g., "2020-01-01"
+        offset_hour: time offset in hours, for example, if this is 3, then it starts from 12-3=9 p.m. instead of 12 a.m.
+
+    Output:
+        start_d: a pandas DatetimeIndex object, indicating the list of starting times
+        end_d: a pandas DatetimeIndex object, indicating the list of ending times
+    """
+    offset_d = pd.Timedelta(offset_hours, unit="h")
+    start_d = pd.date_range(start=start_date_eastern, end=end_date_eastern, closed="left", tz="US/Eastern") - offset_d
+    end_d = start_d + pd.Timedelta(duration, unit="h")
+    #end_d = pd.date_range(start=start_date_eastern, end=end_date_eastern, closed="right", tz="US/Eastern") - offset_d
+    return (start_d, end_d)
+
+# this is fine
+def get_time_range_list(start_date_str_list, duration=24, offset_hours=3):
+    """
+    Convert lists of starting and ending date strings to objects
+
+    Input:
+        start_date_str_list: a list of date strings, e.g., ["2019-04-23", "2019-12-22", "2020-02-05"]
+        duration: the number of hours for each time range, e.g., 24
+        offset_hour: time offset in hours, for example, if this is 3, then it starts from 12-3=9 p.m. instead of 12 a.m.
+
+    Output:
+        start_d: a pandas DatetimeIndex object, indicating the list of starting times
+        end_d: a pandas DatetimeIndex object, indicating the list of ending times
+    """
+    offset_d = pd.Timedelta(offset_hours, unit="h")
+    start_d = pd.DatetimeIndex(data=start_date_str_list, tz="US/Eastern") - offset_d
+    end_d = start_d + pd.Timedelta(duration, unit="h")
+    return (start_d, end_d)
+
+# this I probably need but needs to be edited significantly, or maybe don't need at all?
+def generate_metadata(start_d, end_d, video_start_delay_hrs=0, url_partition=4, redo=0,
+        prefix="banana_", add_smell=True, lat="40.42532", lng="-79.91643", zoom="9.233", credits="CREATE Lab",
+        category="Plume Viz", name_prefix="PARDUMP ", file_path="https://cocalc-www.createlab.org/test/"):
+    """
+    Generate the EarthTime layers and the thumbnail server urls that can be called later to obtain video frames
+
+    Input:
+        start_d: a pandas DatetimeIndex object, indicating the list of starting times
+        end_d: a pandas DatetimeIndex object, indicating the list of ending times
+        video_start_delay_hrs: number of hours that the model should run before the video starts
+        url_partition: the number of partitions for the thumbnail server request for getting images of video frames
+        img_size: the size of the output video (e.g, 540 means 540px for both width and height)
+        redo: this is a number to force the server to avoid using the cached file
+        prefix: a string prefix for the generated unique share url identifier in the EarthTime layers
+        add_smell: a flag to control if you want to add the smell reports to the visualization
+        lat: a string that indicates the latitude of the EarthTime base map
+        lng: a string that indicates the longitude of the EarthTime base map
+        zoom: a string that indicates the zoom level of the EarthTime base map
+        credits: a string to fill out the "Credits" column in the output EarthTime layers file
+        category: a string to fill out the "Category" column in the output EarthTime layers file
+        name_prefix: a string predix for the "Name" column in the output EarthTime layers file
+        file_path: an URL path to indicate the location of your hysplit bin files
+
+    Output:
+        df_layer: the pandas dataframe for the EarthTime layer document
+        df_share_url: the pandas dataframe for the share urls
+        df_img_url: the pandas dataframe for the thumbnail server urls to get images of video frames
+        file_name: a list of file names that are used for saving the hysplit bin files
+    """
+    if url_partition < 1:
+        url_partition = 1
+        print("Error! url_partition is less than 1. Set the url_partition to 1 to fix the error.")
+
+    # Create rows in the EarthTime layer document
+    df_template = pd.read_csv("data/earth_time_layer_template.csv")
+    df_layer = pd.concat([df_template]*len(start_d), ignore_index=True)
+    vid_start_d = start_d + pd.DateOffset(hours=video_start_delay_hrs)
+    start_d_utc = vid_start_d.tz_convert("UTC")
+    end_d_utc = end_d.tz_convert("UTC")
+    file_name = prefix + start_d_utc.strftime("%Y%m%d%H%M") + "_" + end_d_utc.strftime("%Y%m%d%H%M")
+    df_layer["Start date"] = start_d_utc.strftime("%Y%m%d%H%M%S")
+    df_layer["End date"] = end_d_utc.strftime("%Y%m%d%H%M%S")
+    df_layer["Share link identifier"] = file_name
+    df_layer["Name"] = name_prefix + start_d_utc.strftime("%Y-%m-%d")
+    df_layer["URL"] = file_path + file_name + ".bin"
+    df_layer["Category"] = category
+    df_layer["Credits"] = credits
+
+    # Create rows of share URLs
+    # et_root_url = "https://headless.earthtime.org/#"
+    # et_part = "v=%s,%s,%s,latLng&ps=2400&startDwell=0&endDwell=0" % (lat, lng, zoom)
+    # ts_root_url = "https://thumbnails-earthtime.cmucreatelab.org/thumbnail?"
+    # ts_part = "&width=%d&height=%d&format=zip&fps=30&tileFormat=mp4&startDwell=0&endDwell=0&fromScreenshot&disableUI&redo=%d" % (img_size, img_size, redo)
+    # share_url_ls = [] # EarthTime share urls
+    # dt_share_url_ls = [] # the date of the share urls
+    # img_url_ls = [] # thumbnail server urls
+    # dt_img_url_ls = [] # the date of the thumbnail server urls
+
+    # NOTE: this part is for testing the new features that override the previous ones
+    df_layer["Vertex Shader"] = "WebGLVectorTile2.particleAltFadeVertexShader"
+    df_layer["Fragment Shader"] = "WebGLVectorTile2.particleAltFadeFragmentShader"
+    et_root_url = "https://headless.earthtime.org/#"
+
+    for i in range(len(start_d_utc)):
+        sdt = start_d_utc[i]
+        edt = end_d_utc[i]
+        # Add the original url
+        sdt_str = sdt.strftime("%Y%m%d%H%M%S")
+        edt_str = edt.strftime("%Y%m%d%H%M%S")
+        #date_str = sdt_str[:8]
+        date_str = sdt_str[:12] + "_" + edt_str[:12]
+        bt = "bt=" + sdt_str + "&"
+        et = "et=" + edt_str + "&"
+        if add_smell:
+            #TODO: create a more stable smell report layer
+            l = "l=mb_labeled,smell_my_city_pgh_reports_top2," + file_name[i] + "&"
+        else:
+            l = "l=mb_labeled," + file_name[i] + "&"
+        # share_url_ls.append(et_root_url + l + bt + et + et_part)
+        # dt_share_url_ls.append(date_str)
+        # Add the thumbnail server url
+        time_span = (edt - sdt) / url_partition
+        for j in range(url_partition):
+            std_j = sdt + time_span*j
+            edt_j = std_j + time_span
+            std_j_str = std_j.strftime("%Y%m%d%H%M%S")
+            edt_j_str = edt_j.strftime("%Y%m%d%H%M%S")
+            bt_j = "bt=" + std_j_str + "&"
+            et_j = "et=" + edt_j_str + "&"
+            rt = "root=" + urllib.parse.quote(et_root_url + l + bt_j + et_j + et_part, safe="") + "&"
+            img_url_ls.append(ts_root_url + rt + ts_part)
+            dt_img_url_ls.append(date_str)
+    df_share_url = pd.DataFrame(data={"share_url": share_url_ls, "date": dt_share_url_ls})
+    df_img_url = pd.DataFrame(data={"img_url": img_url_ls, "date": dt_img_url_ls})
+
+    # return the data
+    return (df_layer, df_share_url, df_img_url, file_name)
+
+
+def simulate(start_time_eastern, o_file, sources, emit_time_hrs=0, duration=24, filter_ratio=0.8,
+        useForecast=False):
+    """
+    Run the HYSPLIT simulation
+
+    Input:
+        start_time_eastern: for different dates, use format "2020-03-30 00:00"
+        o_file: file path to save the simulation result, e.g., "/projects/cocalc-www.createlab.org/pardumps/test.bin"
+        sources: dict containing location of the sources of pollution (DispersionSource objects), color, and ratio of points to filter
+        emit_time_hrs: affects the emission time for running each Hysplit model
+        duration: total time (in hours) for the simulation, use 24 for a total day, use 12 for testing
+        filter_ratio: the ratio that the points will be dropped (e.g., 0.8 means dropping 80% of the points)
+        hysplit_root: the root directory of the hysplit software
+    """
+    print("="*100)
+    print("="*100)
+    print("start_time_eastern: %s" % start_time_eastern)
+    print("o_file: %s" % o_file)
+
+    # Check and make sure that the o_file path is created
+    check_and_create_dir(o_file)
+
+    # Run simulation and get the folder list (the generated files are cached)
+    path_list = []
+    for source in sources:
+        path_list += getMultiHourDispersionRunsParallel(
+                source["dispersion_source"],
+                parse_eastern(start_time_eastern),
+                emit_time_hrs,
+                duration,
+                HysplitModelSettings(initdModelType=InitdModelType.ParticleHV, hourlyPardump=False),
+                useForecast=useForecast)
+    print("Simulation done, len(path_list)=%d" % len(path_list))
+
+
+    # Check and make sure that the o_file path is created
+    cdump_txt_path_list = []
+    # description = pd.DataFrame([[f"Model type:ParticleHV, Day:{start_time_eastern}, Duration:{duration}, emission_time_hrs:{emit_time_hrs}"]])
+    cdump_df = pd.DataFrame()
+    # cdump_df = pd.concat([description, cdump_df], axis=0)
+    for folder in path_list:
+        if not findInFolder(folder,"cdump.txt"):
+            cdump = findInFolder(folder, "cdump")
+            cmd = hysplit_root + "exec/con2asc -i%s -s -t -v -x" % cdump
+            if cdump.find('.txt') == -1:
+                cdump_txt_path_list.append(cdump+".txt")
+                cdump_txt = cdump+".txt"
+            subprocess_check(cmd)
+            print("con2asc done for %s" % cdump)
+        else:
+            print("cdump.txt already exists")
+            cdump_txt = findInFolder(folder,'cdump.txt')
+            cdump_txt_path_list.append(cdump_txt)
+
+
+        # extract unique info for the run
+        coord = (folder.split("/")[7].split("_")[0])
+        # connect this to source id?? or just add it to the df
+        datetime = folder.split("/")[-1]
+
+        # cdump_txt = findInFolder(folder,'cdump*.txt')
+        # make a dataframe of txt file
+        df = pd.read_fwf(cdump_txt)
+        df.columns = df.columns.str.replace(r'^TEST.*', 'TEST', regex=True)
+        # add coord as another column
+        df = df.assign(source=coord)
+        # df = df.insert(0, 'source', coord, allow_duplicates=False)
+        # add df to the big df
+        cdump_df = pd.concat([cdump_df, df], axis=0, join='outer')
+
+    # Write the description and data to a CSV file
+    # this is a bit hardcoded, need to be able to change the emission scheme automatically
+    description = f"Model type:ParticleHV,particle dispersion scheme: 1, Day:{start_time_eastern}, Duration:{duration}hrs, emission_time_hrs:{emit_time_hrs}hrs, emission scheme: {sources}  \n \n"
+    # emit1 = specific emission scheme with weighted conc
+    run_info = "run_%s_%shr" % (start_time_eastern, duration)
+    with open('/home/nholzbach/hysplit_data/results/%s.csv' % run_info, 'w') as f:
+        f.write(description)
+        cdump_df.to_csv(f, header=True, index=False)
+
+
+    # print("len(cdump_txt_list)=%d" % len(cdump_txt_path_list))
+    # cdump_df.to_csv('/home/nholzbach/hysplit_data/results/%s.csv' % run_info, index=False)
+    print("cdumps all collected in home/nholzbach/hysplit_data/results/%s.csv" % run_info )
+
+
+    # traj_file_list = []
+    # for folder in path_list:
+    #     traj = findInFolder(folder,"PARTICLE.DAT")
+    #     traj_file_list.append(traj)
+    # print("len(traj_file_list)=%d" % len(traj_file_list))
+
+    print("filtering has been skipped, check around line 267 of automate file")
+    # filter_out_ratios = [source["filter_out"] for source in sources] if "filter_out" in sources[0] else filter_ratio
+    # print("Creating %s" % o_file)
+    # this is where the files are edited and particle data is restructured to visualise
+    # create_multisource_bin(traj_file_list, o_file, len(sources), filter_out_ratios=filter_out_ratios)
+    # print("Created %s" % o_file)
+    # os.chmod(o_file, 0o777)
+
+    # Cleanup files
+    # print("Cleaning files...")
+    # for folder in path_list:
+    #     pdump_txt = findInFolder(folder,'PARDUMP*.txt')
+    #     print("Remove file %s" % pdump_txt)
+    #     os.remove(pdump_txt)
+    #     # TODO: gzip PARDUMP.* files
+
+
+def is_url_valid(url):
+    """Check if the url is valid (has something)"""
+    try:
+        r = requests.head(url)
+        return r.status_code == requests.codes.ok
+    except Exception:
+        traceback.print_exc()
+        return False
+
+
+def simulate_worker(start_time_eastern, o_file, sources, emit_time_hrs, duration, filter_ratio, useForecast=False):
+    """
+    The parallel worker for hysplit simulation
+
+    Input:
+        o_url: if not None, check if the URL for the particle file already exists in the remote server
+        (for other input parameters, see the docstring of the simulate function)
+    """
+    # Skip if the file exists in local
+    if os.path.isfile(o_file):
+        print("File exists in local %s" % o_file)
+        return True
+
+    # # Skip if the file exists in remote
+    # if o_url is not None and is_url_valid(o_url):
+    #     print("File exists in remote %s" % o_url)
+    #     return True
+
+    # Perform HYSPLIT model simulation
+    try:
+        simulate(start_time_eastern, o_file, sources,
+                emit_time_hrs=emit_time_hrs, duration=duration, filter_ratio=filter_ratio, useForecast=useForecast)
+        return True
+    except Exception:
+        print("-"*60)
+        print("Error when creating %s" % o_file)
+        traceback.print_exc()
+        print("-"*60)
+        return False
+
+
+
+def check_and_create_dir(path):
+    """Check if a directory exists, if not, create it"""
+    if path is None: return
+    dir_name = os.path.dirname(path)
+    if dir_name != "" and not os.path.exists(dir_name):
+        try: # this is used to prevent race conditions during parallel computing
+            os.makedirs(dir_name)
+            os.chmod(dir_name, 0o777)
+        except Exception:
+            traceback.print_exc()
+
+
+def del_dir(dir_p):
+    """Delete a directory and all its contents"""
+    if not os.path.isdir(dir_p): return
+    try:
+        shutil.rmtree(dir_p)
+    except Exception:
+        traceback.print_exc()
+
+
+def get_all_file_names_in_folder(path):
+    """Return a list of all files in a folder"""
+    return [f for f in listdir(path) if isfile(join(path, f))]
+
+
+def get_all_dir_names_in_folder(path):
+    """Return a list of all directories in a folder"""
+    return [f for f in listdir(path) if isdir(join(path, f))]
---
+++ snellius_runs/hysplit_data/pardump_util.py
@@ -0,0 +1,259 @@
+"""
+This is the utility function for automating plume visualization
+This code was taken and edited from the following path on the hal21 server:
+    /projects/earthtime/files/air-src/linRegModel/pardump_example/pardumpdump-randy-amy-util.ipynb
+"""
+
+
+import glob, os, array, datetime, dateutil.parser, math, random, re, json
+import numpy as np
+import pandas as pd
+
+from utils import subprocess_check, SimpleProcessPoolExecutor
+
+#adjust epoch to January 1, 2020 and scale from seconds to minutes for greater precision
+EPOCH_OFFSET = 1577836800
+EPOCH_SCALE = 60
+
+def gunzipFiles(fnames, zipfnames):
+    for fname in zipfnames:
+        if fname[:-3] not in fnames:
+            print("Unzipping %s" % fname)
+            cmd = "gunzip -k %s" % (fname)
+            subprocess_check(cmd)
+
+
+def cleanup(fnames):
+    for fname in fnames:
+        if fname.find("gz") != -1:
+            print("Skipping %s" % (fname))
+            continue
+        elif fname.find("txt") != -1:
+            print("Deleting %s" % (fname))
+            cmd = "rm %s" % (fname)
+            subprocess_check(cmd)
+
+
+def findInFolder(folder, pattern):
+    result = glob.glob(os.path.join(folder,pattern))
+    if len(result) > 0:
+        return result[0]
+    else:
+        return False
+
+# this restructures the pardump file in order to generate images. This is what we DON'T want
+def parse_pardump(fname, filter_ratio=0.8, with_size=False):
+    # Process lines in the file
+    print("Process lines...")
+    points = {}
+    with open(fname, "r") as f:
+        for line in f:
+            try:
+                L = []
+                raw_line = line.rstrip().split(" ")
+                for l in raw_line:
+                    if l.rstrip() != '':
+                        if l.find('.') > -1:
+                            l = float(l)
+                        else:
+                            l = int(l)
+                        L.append(l)
+            except:
+                L = line
+            if len(L) == 7:
+                minute = L[6]
+                dt = datetime.datetime(2000 + L[2], L[3], L[4], L[5], L[6])
+                epoch = datetime_to_epoch(dt)
+            elif len(L) == 6:
+                x, y = lonlat_to_pixel_xy((L[1], L[0]))
+                z = float(L[2])
+                if with_size:
+                    sigh = sigh_to_pixel(float(l[3]), l[0])
+            elif len(L) == 5:
+                # This is where we can find the points
+                if random.random() > filter_ratio:
+                    idx = L[4]
+                    if idx not in points:
+                        points[idx] = []
+                    if minute % 5 == 0:
+                        if with_size:
+                            points[idx].append([x, y, z, epoch, sigh])
+                        else:
+                            points[idx].append([x, y, z, epoch])
+    # Process points
+    print("Process %d points obtained from the file" % len(points))
+    # c = pack_color(rgb)
+    data = []
+    for idx in points:
+        p = points[idx]
+        if len(p) > 1:
+            for i in range(0, len(p) - 1):
+                p0 = p[i]
+                p1 = p[i+1]
+                # Each shader record in float32 is:
+                # x0, y0, z0, epoch0, x1, y1, z1, epoch1, packedColor
+                # x and y are in web mercator space 0,0 is NW 255,255 is SE
+                if with_size:
+                    data += [p0[0], p0[1], p0[2], p0[3], p1[0], p1[1], p1[2], p1[3], c, p1[4]]
+                else:
+                    data += [p0[0], p0[1], p0[2], p0[3], p1[0], p1[1], p1[2], p1[3], c]
+    return data
+
+
+def pack_color(color):
+    return color[0] + color[1] * 256.0 + color[2] * 256.0 * 256.0;
+
+
+def lonlat_to_pixel_xy(lonlat):
+    (lon, lat) = lonlat
+    x = (lon + 180.0) * 256.0 / 360.0 #converts [-180,180] to [0,256]
+    y = 128.0 - math.log(math.tan((lat + 90.0) * math.pi / 360.0)) * 128.0 / math.pi
+    return [x, y]
+
+def lonlat_to_pixel_xy_series(lonlat):
+    (lon, lat) = lonlat
+    x = (lon + 180.0) * 256.0 / 360.0 #converts [-180,180] to [0,256]
+    y = 128.0 - np.log(np.tan((lat + 90.0) * math.pi / 360.0)) * 128.0 / math.pi
+    return [x, y]
+
+
+def sigh_to_pixel(sigh,lat):
+    return sigh / (157000.0 * abs(math.cos(lat)))
+
+
+def datetime_to_epoch(dt):
+    return (dt - datetime.datetime(1970, 1, 1)).total_seconds()
+
+
+def scale_particle(sigh):
+    return ((sigh + 1.0) / 12600.0) + 1.0 #offset by 1 to make sure all points draw
+
+
+def create_bin(fnames, o_file, colormap):
+    """Coloring based on hour"""
+    i = 0
+    step = 255/(len(fnames) - 1)
+    points = []
+    for fname in fnames:
+        # rgb = colormap[0][int(i*step)]
+        print("Process %s" % fname)
+        points += parse_pardump(fname)
+        lines = []
+        i += 1
+    array.array('f', points).tofile(open(o_file, 'wb'))
+    points = []
+
+# this is what gets used in other files, 
+def create_multisource_bin(fnames, o_file, numSources, filter_out_ratios=0.8):
+    """
+    filter_out_ratios=0.8 means that 80% of the points will be dropped. if specified as a dict, filter ratios are applied per source.
+    with_size=True means visualizing puffs instead of particles
+    """
+    runTimeHrs = int(len(fnames) / numSources)
+
+    filter_dict = False
+    if type(filter_out_ratios) == list:
+        assert len(filter_out_ratios) == numSources
+        filter_dict = True
+
+    print("Only use %s of all the points to reduce file size" % filter_out_ratios)
+    maxWorkers = 10
+    pool = SimpleProcessPoolExecutor(maxWorkers)
+    for i in range(numSources):
+        start_file = i * runTimeHrs
+        single_source = fnames[start_file:start_file+runTimeHrs]
+        filter_out = filter_out_ratios[i] if filter_dict else filter_out_ratios
+
+        pool.submit(particle_dat_to_bin,single_source,filter_out)
+    
+    all_points = pool.shutdown()
+    points = np.concatenate(all_points)
+
+    #construct subset dir
+
+    #sort by first timestamp
+    points = points[points[:,3].argsort()]
+    tstamps = points[:,3]
+
+    subsets = []
+    first = int(min(tstamps))
+    last = int(max(tstamps))
+    last_index = 0
+
+    for t in range(first + 60,last,60):
+        subset_record = {}
+        next_index = np.where(tstamps == t)[0][0]
+        subset_record["epoch"] = int(datetime.datetime.fromtimestamp(tstamps[next_index] * EPOCH_SCALE + EPOCH_OFFSET).timestamp())
+        subset_record["first"] = int(last_index)
+        subset_record["count"] = int(next_index - last_index)
+        
+        subsets.append(subset_record)
+        last_index = next_index
+    
+    with open(o_file[:-4] + ".json", 'w') as f:
+        json.dump(subsets, f)
+    
+    print("Writing array to file %s" % o_file)
+    points.tofile(o_file)
+
+
+def read_particle_dat(particle_dat_filename, filter_out = .5, subsample=10 ):
+    pardump_df = pd.read_csv(particle_dat_filename, delim_whitespace=True)
+    print(f'Read {len(pardump_df)} records')
+
+    pardump_df.sort_values(['index', 'time'], inplace=True)
+    min_times = pardump_df.groupby(by='index')['time'].min()
+    pardump_df = pd.merge(left=pardump_df,right=min_times,on='index',suffixes=['','_min'])
+    pardump_df = pardump_df[pardump_df['time'].mod(subsample) == pardump_df['time_min'].mod(subsample)]
+
+    print(f'{len(pardump_df)} records after subsampling time by {subsample}')
+
+    assert(filter_out < 1)
+    keep_ratio = 1 - filter_out
+    num_particles = pardump_df['index'].max()
+    keep_indxs = random.sample(range(1,num_particles),int(num_particles * keep_ratio))
+    pardump_df = pardump_df[pardump_df['index'].isin(keep_indxs)]
+    print(f'{len(pardump_df)} records after dropping {filter_out * 100}% of particle indices')
+
+    pardump_df['time'] = pardump_df['time'] * 60
+    start_datetime = dateutil.parser.parse(re.search(
+        r'\d{8}_\d{6}-\d{4}',particle_dat_filename).group(0).replace('_',' ')).timestamp()
+    pardump_df['time'] = pardump_df['time'] + start_datetime
+    print(start_datetime)
+    pardump_df.index = range(0, len(pardump_df))
+    return pardump_df
+
+def read_and_concat_particle_dat_files(filenames,filter_out):
+    dfs = []
+    index_offset = 0
+
+    for filename in filenames:
+        df: pd.DataFrame = read_particle_dat(filename,filter_out)
+        df['index'] += index_offset
+        print(f'Read {len(df)} records, indices {df["index"].min()} to {df["index"].max()}, from {filename}')
+        index_offset = df['index'].max()
+        dfs.append(df)
+
+    df = pd.concat(dfs)
+    print(f'Concatenated to {len(df)} records, indices {df["index"].min()} to {df["index"].max()}')
+    return df
+
+def df_to_bin(pardump_df):
+    index = pardump_df['index'].to_numpy()
+    lat = pardump_df['lat'].to_numpy()
+    lon = pardump_df['lon'].to_numpy()
+    alt = pardump_df['agl'].to_numpy()
+    epochtime = ((pardump_df['time'] - EPOCH_OFFSET) / EPOCH_SCALE ).to_numpy()
+    # packed_color = pack_color(rgb)
+
+    x, y = lonlat_to_pixel_xy_series((lon, lat))
+
+    newdf = pd.DataFrame({'particle_id0':index[:-1],'x0': x[:-1], 'y0': y[:-1], 'z0': alt[:-1], 't0': epochtime[:-1],
+                         'particle_id1':index[1:],'x1': x[1:], 'y1': y[1:], 'z1': alt[1:], 't1': epochtime[1:]})
+    records = newdf[newdf.particle_id0 == newdf.particle_id1]
+    return records.drop(['particle_id0','particle_id1'],axis=1).to_numpy(np.float32)
+
+def particle_dat_to_bin(filenames,filter_out):
+    df = read_and_concat_particle_dat_files(filenames,filter_out)
+    df.reset_index(inplace=True)
+    return df_to_bin(df)
